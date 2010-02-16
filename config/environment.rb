d256472a08f1d6e29543adc4c4fb72f5fa88d7cc
Peter Haza <peter.haza@gmail.com>
2010-02-16 01:29:00 +0100

    Add formtastic

--- config/environment.rb
+++ config/environment.rb
@@ -16,6 +16,7 @@ Rails::Initializer.run do |config|
   config.gem 'shoulda'
   config.gem 'factory_girl'
   config.gem 'nokogiri'
+  config.gem 'formtastic'
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
