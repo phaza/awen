92d04b782898205a1b9009c9a035515f7a3a5ad0
Peter Haza <peter.haza@gmail.com>
2010-02-18 23:35:14 +0100

    Add incoming folder as default and refactor folder generation code

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -1,8 +1,9 @@
 Settings.defaults[:media_folder]    = "#{RAILS_ROOT}/media"
+Settings.defaults[:incoming_folder] = "#{Settings.media_folder}/incoming"
 Settings.defaults[:music_folder]    = "#{Settings.media_folder}/music"
 Settings.defaults[:video_folder]    = "#{Settings.media_folder}/video"
 Settings.defaults[:podcast_folder]  = "#{Settings.media_folder}/podcast"
 
-FileUtils.makedirs Settings.music_folder unless File.exists? Settings.music_folder
-FileUtils.makedirs Settings.video_folder unless File.exists? Settings.video_folder
-FileUtils.makedirs Settings.podcast_folder unless File.exists? Settings.podcast_folder
+Settings.defaults.find_all{|k, v| k.to_s.include? 'folder'}.each do |setting, folder|
+  FileUtils.makedirs folder unless File.exists? folder
+end
