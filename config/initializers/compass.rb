11646048b7d4db9c266bb7fb31d562bc4049cfa1
Peter Haza <peter.haza@gmail.com>
2010-03-28 19:56:30 +0200

    Fix file indentation

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,6 +1,6 @@
 require 'compass'
-  # If you have any compass plugins, require them here.
-  Compass.add_project_configuration(File.join(RAILS_ROOT, "config", "compass.rb"))
-  Compass.configuration.environment = (defined?(Rails) ? Rails.env : RAILS_ENV).to_sym
-  Compass.configure_sass_plugin!
+# If you have any compass plugins, require them here.
+Compass.add_project_configuration(File.join(defined?(Rails) ? Rails.root : RAILS_ROOT, "config", "compass.rb"))
+Compass.configuration.environment = (defined?(Rails) ? Rails.env : RAILS_ENV).to_sym
+Compass.configure_sass_plugin!
   
