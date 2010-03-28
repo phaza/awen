362440b050f489802486e18c126f0d505b0921b8
Peter Haza <peter.haza@gmail.com>
2010-03-28 19:55:03 +0200

    No need to explicitly set cache_location as it was fixed in pre6

--- config/compass.rb
+++ config/compass.rb
@@ -11,4 +11,3 @@ css_dir = "public/stylesheets/compiled"
 sass_dir = "app/stylesheets"
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
-      sass_options = {:cache_location => "/tmp/sass-cache"}
