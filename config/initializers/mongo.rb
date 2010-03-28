50659270ecc158bb973b4ac24347c6249d795fc9
Peter Haza <peter.haza@gmail.com>
2010-03-28 14:07:56 +0200

    Mongodb connection

---
+++ config/initializers/mongo.rb
@@ -0,0 +1,8 @@
+db_config = YAML::load(File.read(Rails.root + "/config/database.yml"))
+
+if db_config[Rails.env] && 
+db_config[Rails.env]['adapter'] == 'mongodb'
+  mongo = db_config[Rails.env]
+  MongoMapper.connection = Mongo::Connection.new(mongo['hostname'])
+  MongoMapper.database = mongo['database']
+end
