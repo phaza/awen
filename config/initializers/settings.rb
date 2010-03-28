049421e6eac79d94c2818fb03416ddaa9bffd0a3
Peter Haza <peter.haza@gmail.com>
2010-03-28 19:53:35 +0200

    Remove trailing file from rails-settings plugin

--- config/initializers/settings.rb
+++
@@ -1,9 +0,0 @@
-Settings.defaults[:media_folder]    = "#{RAILS_ROOT}/media"
-Settings.defaults[:incoming_folder] = "#{Settings.media_folder}/incoming"
-Settings.defaults[:music_folder]    = "#{Settings.media_folder}/music"
-Settings.defaults[:video_folder]    = "#{Settings.media_folder}/video"
-Settings.defaults[:podcast_folder]  = "#{Settings.media_folder}/podcast"
-
-Settings.defaults.find_all{|k, v| k.to_s.include? 'folder'}.each do |setting, folder|
-  FileUtils.makedirs folder unless File.exists? folder
-end
